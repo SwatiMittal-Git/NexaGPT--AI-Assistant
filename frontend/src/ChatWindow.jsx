76072d3ca614f9fdaa8005b524aed442223c7451
Swati <swatimittal7819@gmail.com>
2026-01-31 16:10:24 +0530

    replace localhost with prod api url

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -26,7 +26,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response =  axios.get(`${import.meta.env.VITE_API_URL}/api/xyz`)
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
